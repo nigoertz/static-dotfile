0f0efceb3ba92ba8868e7d11b59a1f54e06a31bb
lutzer <in_the_fishtank@gmx.de>
2021-05-28 16:39:56 +0200

    fixed path error

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 
 # Use
 
-Input the directory path that will act as the root in the input field `folder`. NOTE: make sure to put a wildcard in the HTTP/HTTPS node path after the root url path, so that the file / directory names are passed to the static node.
+Input the directory path that will act as the root in the input field `folder`. NOTE: make sure to put a wildcard in the HTTP/HTTPS node path after the root url path, so that the file / directory names are passed to the static node. e.g.: `prefix/*`
 
 <br/>
 <br/>
--- static.js
+++ static.js
@@ -1,25 +1,22 @@
-let serveStatic = require("serve-static");
+// const fs = require('fs')
+// const path = require('path')
+const serveStatic = require('serve-static')
 
 let StaticInitFunc = function (RED) {
   function StaticNode(n) {
-    RED.nodes.createNode(this, n);
+    RED.nodes.createNode(this, n)
 
-    var node = this;
-    var folder = n.folder;
-    var serve = serveStatic(folder, {
-      index: ["index.html", "index.htm"],
-    });
+    const node = this
+    const folder = n.folder
+    
+    const serve = serveStatic(folder, { index: ["index.html", "index.htm"] })
+
 
     node.on("input", function (msg) {
-      let {
-        req,
-        res: { _res },
-      } = msg;
-      let _req = Object.assign({}, req);
-      _req.pathname = _req.path = _req.url = `/${req.path
-        .replace(req.baseUrl, "")
-        .replace(req.path.match(_req.route.path)[0], "")}`;
-      serve(_req, _res, function () {
+      const req = msg.req
+      const res = msg.res._res
+      req.pathname = req.path = req.url = `/${Object.values(req.params)[0]}`
+      serve(req, res, function (t) {
         node.send(msg);
       });
     });
