1e95ff625372b91a653770d8c837d2a8b08d85c7
tj <tj@digitalarsenal.io>
2022-08-24 14:31:55 -0400

    updates

--- static.js
+++ static.js
@@ -1,24 +1,25 @@
-const serveStatic = require('serve-static')
+let serveStatic = require("serve-static");
 
 let StaticInitFunc = function (RED) {
   function StaticNode(n) {
-    RED.nodes.createNode(this, n)
+    RED.nodes.createNode(this, n);
 
-    const node = this
-    const folder = n.folder
-    
-    const serve = serveStatic(folder, { index: ["index.html", "index.htm"] })
+    var node = this;
+    var folder = n.folder;
+    var serve = serveStatic(folder, {
+      index: ["index.html", "index.htm"],
+    });
 
     node.on("input", function (msg) {
-      const req = msg.req
-      const res = msg.res._res
-      req.pathname = req.path = req.url = `/${Object.values(req.params)[0]}`
-      serve(req, res, function (t) {
-        node.send(msg)
+            msg.req.pathname = msg.req.path = msg.req.url = `/${msg.req.path
+        .replace(msg.req.baseUrl, "")
+        .replace(msg.req.path.match(msg.req.route.path)[0], "")}`;
+      serve(msg.req, msg.res._res, function () {
+        node.send(msg);
       });
     });
   }
-  RED.nodes.registerType("static", StaticNode)
+  RED.nodes.registerType("static", StaticNode);
 };
 
-module.exports = StaticInitFunc
+module.exports = StaticInitFunc;
